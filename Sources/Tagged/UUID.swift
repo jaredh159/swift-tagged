cb4284055f6e9cc5576ff9be13723b3a039905a5
Jared Henderson <jaredh159@users.noreply.github.com>
2023-01-04 16:49:20 -0500

    remove empty initializer for UUID (for Duet)

--- Sources/Tagged/UUID.swift
+++ Sources/Tagged/UUID.swift
@@ -1,13 +1,6 @@
 import Foundation
 
 extension Tagged where RawValue == UUID {
-  /// Generates a tagged UUID.
-  ///
-  /// Equivalent to `Tagged<Tag, _>(UUID(())`.
-  public init() {
-    self.init(UUID())
-  }
-
   /// Creates a tagged UUID from a string representation.
   ///
   /// - Parameter string: The string representation of a UUID, such as
